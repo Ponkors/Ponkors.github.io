43a582a64b6e96ac40c02d158c3fe8c65d768d9b
Ponkors <Ponkors@users.noreply.github.com>
2025-04-25 10:00:25 +0000

    deploy: 961fb2e9d2b802ae288d0a6d6b063fce3813ea84

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3518732825"
+    serviceWorkerVersion: "1476471918"
   }
 });
